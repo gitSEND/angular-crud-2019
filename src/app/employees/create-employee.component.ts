f39b4be5e588853362447e6124a2c473104bc5b3
NelsonDSanchezE <n.sanchezesparza@gmail.com>
2019-11-16 10:24:13 -0500

    angular select options from array

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model';
 
 @Component({
   selector: 'app-create-employee',
@@ -8,8 +9,15 @@ import { NgForm } from '@angular/forms';
 })
 export class CreateEmployeeComponent implements OnInit {
   // gender = 'male';
-  isActive = true;
-  department = '3';
+  // isActive = true;
+  // department = '3';
+
+  departments: Department[] = [
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'IT' },
+    { id: 4, name: 'Payroll' }
+  ];
   constructor() { }
 
   ngOnInit() {
@@ -18,6 +26,6 @@ export class CreateEmployeeComponent implements OnInit {
   saveEmployee(empForm: NgForm): void {
     // console.log(empForm.value);
     console.log(empForm);
-    
+
   }
 }
