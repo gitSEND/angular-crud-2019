29f0e69f8fdf1437b9933e4577cb1bdd664df8e8
NelsonDSanchezE <n.sanchezesparza@gmail.com>
2019-11-16 09:56:53 -0500

    angular radio button checked by default

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -7,7 +7,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-
+  // gender = 'male';
   constructor() { }
 
   ngOnInit() {
