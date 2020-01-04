f9b2e64004780ff8a029c5612d13c5350d3b6cfe
NelsonDSanchezE <n.sanchezesparza@gmail.com>
2020-01-04 12:32:01 -0500

    Angular component input property change detection

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Employee } from '../models/employee.model';
 
 @Component({
@@ -6,8 +6,8 @@ import { Employee } from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit {
-// Parent component will use this Input property to pass
+export class DisplayEmployeeComponent implements OnInit, OnChanges {
+  // Parent component will use this Input property to pass
   // the employee object to which the template binds to
   @Input() employee: Employee;
 
@@ -16,4 +16,15 @@ export class DisplayEmployeeComponent implements OnInit {
   ngOnInit() {
   }
 
+  // This life cycle hook receives SimpleChanges as an Input parameter
+  // We can use it to retrieve previous and current values as shown below
+  ngOnChanges(changes: SimpleChanges) {
+    // console.log(changes);
+    
+    const previousEmployee = <Employee>changes.employee.previousValue;
+    const currentEmployee = <Employee>changes.employee.currentValue;
+
+    console.log('Previous : ' + (previousEmployee ? previousEmployee.name : 'NULL'));
+    console.log('Current : ' + currentEmployee.name);
+  }
 }
