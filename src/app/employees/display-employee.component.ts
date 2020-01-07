ca7038fe512431623e0ecaedc189fdc319732399
NelsonDSanchezE <n.sanchezesparza@gmail.com>
2020-01-06 23:23:37 -0500

    Angular optional route parameters

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Employee } from '../models/employee.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-display-employee',
@@ -9,8 +10,12 @@ import { Employee } from '../models/employee.model';
 export class DisplayEmployeeComponent implements OnInit {
   
   @Input() employee: Employee;
-  constructor() { }
+
+  selectedEmployeeId: number;
+
+  constructor(private _route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.selectedEmployeeId = +this._route.snapshot.paramMap.get('id');
   }
 }
