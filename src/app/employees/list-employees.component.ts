9ae8e30a804b1daa3a9d1895703c06f40865c2b3
NelsonDSanchezE <n.sanchezesparza@gmail.com>
2020-01-11 18:05:28 -0500

    Data filtering in angular component

--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -10,27 +10,50 @@ import { Router } from '@angular/router';
 export class ListEmployeesComponent implements OnInit {
 
   employees: Employee[];
+  // Use this property to stored filtered employees, so we do not
+  // lose the original list and do not have to make a round trip
+  // to the web server on every new search
+  filteredEmployees: Employee[];
   dataFromChild: Employee;
-  searchTerm: string;
+
+  private _searchTerm: string;
+  // We are binding to this property in the view template, so this
+  // getter is called when the binding needs to read the value
+  get searchTerm(): string {
+    return this._searchTerm;
+  }
+  // This setter is called everytime the value in the search text box changes
+  set searchTerm(value: string) {
+    this._searchTerm = value;
+    this.filteredEmployees = this.filterEmployees(value);
+  }
+
+  filterEmployees(searchString: string) {
+    return this.employees.filter(employee =>
+      employee.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1);
+  }
+
   constructor(private _employeeService: EmployeeService, private _router: Router) { }
 
   ngOnInit() {
     this.employees = this._employeeService.getEmployees();
+    this.filteredEmployees = this.employees;
   }
 
-  onClick(employeeId: number){
+  onClick(employeeId: number) {
     this._router.navigate(['/employees', employeeId])
   }
 
   changeEmployeeName() {
-    // this.employees[0].name = 'Jordan';
-    const newEmployeeArray: Employee[] = Object.assign([], this.employees);
-    newEmployeeArray[0].name = 'Jordan';
-    this.employees = newEmployeeArray;
+    this.employees[0].name = 'Jordan';
+    this.filteredEmployees = this.filterEmployees(this.searchTerm);
+    // const newEmployeeArray: Employee[] = Object.assign([], this.employees);
+    // newEmployeeArray[0].name = 'Jordan';
+    // this.employees = newEmployeeArray;
   }
 
-  onMouseMove(){
-    
+  onMouseMove() {
+
   }
 
 }
