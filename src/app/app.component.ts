cb938d6720d64cc7ccbbb5ed2ed3cc1cda08ec5d
NelsonDSanchezE <n.sanchezesparza@gmail.com>
2020-01-18 19:58:09 -0500

    Angular route loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+// Import the Router and navigation events
+import {
+  Router, NavigationStart, NavigationEnd,
+  NavigationCancel, NavigationError, Event
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-angular-crud';
+
+  // We will use this property to show or hide
+  // the loading indicator
+  showLoadingIndicator = true;
+
+  // Inject the Angular Router
+  constructor(private _router: Router) {
+    // Subscribe to the router events observable
+    this._router.events.subscribe((routerEvent: Event) => {
+
+      // On NavigationStart, set showLoadingIndicator to ture
+      if (routerEvent instanceof NavigationStart) {
+        this.showLoadingIndicator = true;
+      }
+
+      // On NavigationEnd or NavigationError or NavigationCancel
+      // set showLoadingIndicator to false
+      if (routerEvent instanceof NavigationEnd ||
+        routerEvent instanceof NavigationError ||
+        routerEvent instanceof NavigationCancel) {
+        this.showLoadingIndicator = false;
+      }
+
+    });
+  }
 }
